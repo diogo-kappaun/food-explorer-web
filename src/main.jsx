81ee564c5e3b8d2f002f6150dc71fa44d1f85923
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-20 15:05:32 -0300

    :boom: fix: Add Toast component in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import 'react-toastify/dist/ReactToastify.css'
 import './global.css'
 import './utils/theme'
 
-import { ToastContainer } from 'react-toastify'
+import { Toast } from './components/Toast'
 import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Routes />
-    <ToastContainer limit={3} hideProgressBar />
+    <Toast />
   </React.StrictMode>,
 )
