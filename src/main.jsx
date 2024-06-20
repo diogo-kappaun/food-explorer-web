42ed5bc775b6e88921e59ec3e36426057d259402
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-20 09:00:55 -0300

    :sparkles: feat: Add Toastify configs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import 'react-toastify/dist/ReactToastify.css'
 import './global.css'
 import './utils/theme'
 
+import { ToastContainer } from 'react-toastify'
 import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Routes />
+    <ToastContainer limit={3} hideProgressBar />
   </React.StrictMode>,
 )
