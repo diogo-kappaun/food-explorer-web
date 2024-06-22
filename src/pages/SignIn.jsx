d8aad79b92e7fa1dab679cdffa733675ad43044e
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-22 20:43:08 -0300

    :sparkles: feat: Add signin logic

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,6 @@
-import { PiEnvelopeSimple, PiEye, PiLockKey } from 'react-icons/pi'
+import { useState } from 'react'
+import { PiEye, PiEyeClosed } from 'react-icons/pi'
+import { toast } from 'sonner'
 
 import * as Form from '../components/Form'
 import * as Input from '../components/Input'
@@ -7,8 +9,34 @@ import { Button } from '../components/Button'
 import { ButtonText } from '../components/ButtonText'
 import { Logo } from '../components/Logo'
 import { ThemeToggler } from '../components/ThemeToggler'
+import { useAuth } from '../hooks/auth'
 
 export function SignIn() {
+  const { signIn } = useAuth()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [isVisible, setIsVisible] = useState(false)
+  const [isRunning, setIsRunning] = useState(false)
+
+  function handleSignIn() {
+    setIsRunning(true)
+
+    if (!email || !password) {
+      setIsRunning(false)
+      return toast.error('Preencha todos os campos!')
+    }
+
+    signIn({ email, password })
+
+    setIsRunning(false)
+  }
+
+  function handlePasswordView() {
+    setIsVisible(!isVisible)
+  }
+
   return (
     <div className="h-screen">
       <img src="" alt="" />
@@ -28,13 +56,11 @@ export function SignIn() {
           <Form.Field>
             <Form.Label htmlFor="email">Endereço de e-mail</Form.Label>
             <Input.Root>
-              <Input.Prefix>
-                <PiEnvelopeSimple size={16} />
-              </Input.Prefix>
               <Input.Control
                 id="email"
                 type="text"
                 placeholder="Insira seu e-mail"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </Input.Root>
           </Form.Field>
@@ -42,23 +68,31 @@ export function SignIn() {
           <Form.Field>
             <Form.Label htmlFor="password">Senha</Form.Label>
             <Input.Root>
-              <Input.Prefix>
-                <PiLockKey size={16} />
-              </Input.Prefix>
               <Input.Control
                 id="password"
-                type="password"
+                type={isVisible ? 'text' : 'password'}
                 placeholder="Insira sua senha"
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <Input.Prefix>
-                <PiEye size={16} />
+              <Input.Prefix className="cursor-pointer">
+                {password ? (
+                  isVisible ? (
+                    <PiEye size={18} onClick={handlePasswordView} />
+                  ) : (
+                    <PiEyeClosed size={18} onClick={handlePasswordView} />
+                  )
+                ) : (
+                  ''
+                )}
               </Input.Prefix>
             </Input.Root>
           </Form.Field>
 
           <ButtonText>Esqueci minha senha</ButtonText>
 
-          <Button>Continuar</Button>
+          <Button disabled={isRunning} form="signup" onClick={handleSignIn}>
+            Continuar
+          </Button>
 
           <span className="flex flex-wrap items-center justify-center gap-1 text-sm text-card-foreground">
             Ainda não tem uma conta?{' '}
