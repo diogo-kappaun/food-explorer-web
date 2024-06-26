91a9ce08880a2b408350d1ca9725afd20c2c0e55
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-26 17:19:19 -0300

    :sparkles: feat: Add settings route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
+
 import { Home } from '../pages/Home'
+import { Settings } from '../pages/Settings'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   )
 }
