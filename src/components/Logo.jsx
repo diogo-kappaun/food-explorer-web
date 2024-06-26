cb25623327b1a6b000a9da9463502f4b90975dd8
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-25 21:36:48 -0300

    :recycle: refactor: Add props in component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import LogoImage from '../assets/logo.svg'
 
-export function Logo({ withText = false, className }) {
+export function Logo({ withText = false, className, props }) {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2" {...props}>
       <img src={LogoImage} alt="logo" className={className} />
       {withText ? (
         <span className="text-lg font-bold text-foreground">Food Explorer</span>
