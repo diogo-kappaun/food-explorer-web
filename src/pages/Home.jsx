a4f8a4921286c88338b106fb66b0645b910f2524
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-26 20:29:02 -0300

    :sparkles: feat: Create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
+
 export function Home() {
   return (
-    <div className="flex h-screen items-center justify-center bg-background text-foreground">
-      <h1>Oi</h1>
+    <div className="min-h-screen bg-background text-foreground lg:grid lg:grid-cols-page lg:grid-rows-page">
+      <Header />
+      <Sidebar />
+
+      <div>
+        <h1>Home!</h1>
+      </div>
     </div>
   )
 }
