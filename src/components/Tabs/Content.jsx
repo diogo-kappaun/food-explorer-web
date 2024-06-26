8264330060bc641ec3f18f7384c079cbba47d9ac
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-26 20:28:16 -0300

    :sparkles: feat: Create tab content component

---
+++ src/components/Tabs/Content.jsx
@@ -0,0 +1,9 @@
+import * as Tabs from '@radix-ui/react-tabs'
+
+export function TabContent({ value, children }) {
+  return (
+    <Tabs.Content value={value} className="h-full py-6">
+      {children}
+    </Tabs.Content>
+  )
+}
