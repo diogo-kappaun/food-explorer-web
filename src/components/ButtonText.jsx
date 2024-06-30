6dccf2d0d39f79ed356828dcd3f45965e19ca41d
diogo-kappaun <diogohkappaun@gmail.com>
2024-06-30 10:47:02 -0300

    :boom: fix: Add twMerge

--- src/components/ButtonText.jsx
+++ src/components/ButtonText.jsx
@@ -1,10 +1,15 @@
+import { twMerge } from 'tailwind-merge'
+
 import { Link } from 'react-router-dom'
 
 export function ButtonText(props) {
   return (
     <Link
       {...props}
-      className="text-sm font-medium text-primary transition-colors hover:text-primary/90 hover:underline"
+      className={twMerge(
+        'hover:underline" text-sm font-medium text-primary transition-colors hover:text-primary/90',
+        props.className,
+      )}
     />
   )
 }
