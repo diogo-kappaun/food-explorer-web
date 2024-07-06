4386e8a085af1dc2ff6f269c1c7d72030f1c3390
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-06 11:50:38 -0300

    :sparkles: feat: Add ButtonBack in settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,3 +1,4 @@
+import { BackButton } from '../components/BackButton'
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
 import { SettingsTabs } from '../components/Tabs'
@@ -9,7 +10,9 @@ export function Settings() {
       <Sidebar />
 
       <div className="px-8 pb-12 pt-24 lg:pt-8">
-        <h1 className="text-3xl font-medium">Configurações</h1>
+        <BackButton />
+
+        <h1 className="mt-3 text-3xl font-medium">Configurações</h1>
 
         <SettingsTabs />
       </div>
