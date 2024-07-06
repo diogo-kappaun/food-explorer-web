4d4e45080b13fb0e66b14012d143712312314281
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-06 11:51:20 -0300

    :lipstick: fix: Remove hover underline

--- src/components/ButtonText.jsx
+++ src/components/ButtonText.jsx
@@ -2,13 +2,13 @@ import { twMerge } from 'tailwind-merge'
 
 import { Link } from 'react-router-dom'
 
-export function ButtonText(props) {
+export function ButtonText({ className, ...props }) {
   return (
     <Link
       {...props}
       className={twMerge(
-        'hover:underline" rounded-md text-sm font-medium text-primary transition-colors hover:text-primary/90',
-        props.className,
+        'rounded-md text-sm font-medium text-primary transition-colors hover:text-primary/90',
+        className,
       )}
     />
   )
