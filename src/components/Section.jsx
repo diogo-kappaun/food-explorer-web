1c400960c816bb4bfea42d5cfa63702eecf218a1
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-08 09:11:39 -0300

    :lipstick: fix: Adjust padding

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,3 +1,5 @@
 export function Section({ children }) {
-  return <div className="space-y-6 px-8 pb-12 pt-24 lg:pt-8">{children}</div>
+  return (
+    <div className="space-y-6 px-4 pb-12 pt-24 lg:px-8 lg:pt-8">{children}</div>
+  )
 }
