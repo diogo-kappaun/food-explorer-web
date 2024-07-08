3ea512823dc697b61faf63a2198dd7fc7b3c4b45
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-08 14:26:36 -0300

    :recycle: refactor: Add props to father element

--- src/components/NewIngredient.jsx
+++ src/components/NewIngredient.jsx
@@ -1,10 +1,10 @@
 import { PiX } from 'react-icons/pi'
 
-export function NewIngredient({ value, onClick, key }) {
+export function NewIngredient({ value, onClick, ...props }) {
   return (
     <div
       className="flex h-10 flex-wrap items-center justify-start gap-3 rounded-md border border-input px-3 py-1 text-sm shadow-sm"
-      key={key}
+      {...props}
     >
       <span>{value}</span>
       <PiX className="cursor-pointer" onClick={onClick} />
