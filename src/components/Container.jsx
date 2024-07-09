180d46f01e186c4d293dca5d33639dc26f1cd709
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-09 09:48:39 -0300

    :lipstick: Adjust content responsiveness with sidebar

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 export function Container({ children }) {
   return (
-    <div className="min-h-screen bg-background text-foreground lg:grid lg:grid-cols-page lg:grid-rows-page">
+    <div className="h-screen bg-background text-foreground lg:grid lg:grid-cols-page lg:grid-rows-page">
       {children}
     </div>
   )
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,7 @@
 export function Section({ children }) {
   return (
-    <div className="space-y-6 px-4 pb-12 pt-24 lg:px-8 lg:pt-8">{children}</div>
+    <div className="space-y-6 overflow-auto px-4 pb-12 pt-24 lg:px-8 lg:pt-8">
+      {children}
+    </div>
   )
 }
