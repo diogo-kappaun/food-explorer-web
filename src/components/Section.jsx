b92393c45a568ef895b8659d5c7f2fe6c66e99c1
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-18 11:04:23 -0300

    :recycle: refactor: Add another div to component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,3 +1,7 @@
 export function Section({ children }) {
-  return <div className="mx-auto w-page space-y-6">{children}</div>
+  return (
+    <div className="overflow-auto px-8 pb-12 pt-24 lg:pt-8">
+      <div className="mx-auto w-page space-y-6">{children}</div>
+    </div>
+  )
 }
