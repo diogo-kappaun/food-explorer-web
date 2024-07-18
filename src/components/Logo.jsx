fd462c2a5e1fb466d12b8c02a35c76afd86bd159
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-18 13:44:49 -0300

    :sparkles: feat: Add Admin logo only user admin

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,24 @@
+import { useAuth } from '../hooks/auth'
+import { USER_ROLE } from '../utils/roles'
+
+import { twMerge } from 'tailwind-merge'
+
 import LogoImage from '../assets/logo.svg'
 
-export function Logo({ withText = false, className, props }) {
+export function Logo({ className, props }) {
+  const { role } = useAuth()
   return (
-    <div className="flex items-center gap-2" {...props}>
-      <img src={LogoImage} alt="logo" className={className} />
-      {withText ? (
-        <span className="text-lg font-bold text-foreground">Food Explorer</span>
-      ) : (
-        ''
+    <div
+      className={twMerge(
+        'flex items-center gap-2 lg:absolute lg:left-[72px] lg:w-6',
+        className,
+      )}
+      {...props}
+    >
+      <img src={LogoImage} alt="logo" />
+
+      {role === USER_ROLE.ADMIN && (
+        <span className="text-base font-normal text-foreground">Admin</span>
       )}
     </div>
   )
