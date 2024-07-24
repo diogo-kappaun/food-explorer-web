bd36475e94eb1c3f9032545d69f5ee187d32f4a3
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-23 21:54:32 -0300

    :wastebasket: remove: Delete formatPrice function

--- src/utils/formatPrice.js
+++
@@ -1,9 +0,0 @@
-export function formatPrice(priceInCents) {
-  const pricePerHundred = priceInCents / 100
-
-  const decimalPrice = pricePerHundred.toFixed(2).toString().replace('.', ',')
-
-  const formattedPrice = decimalPrice.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-
-  return formattedPrice
-}
