7196592e1169ac9aaa30412c52f524f963486b15
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-26 20:18:54 -0300

    :sparkles: feat: Create favorites hook

---
+++ src/hooks/favorites.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+import { api } from '../services/api'
+
+export const FavoriteContext = createContext()
+
+export function FavoriteProvider({ children }) {
+  const [favorites, setFavorites] = useState([])
+
+  async function getFavorites() {
+    const { data } = await api.get('/favorites')
+
+    setFavorites(data.favorites)
+  }
+
+  async function toggle({ dishId }) {
+    const { data } = await api.post(`favorites?dish_id=${dishId}`)
+
+    setFavorites(data)
+  }
+
+  useEffect(() => {
+    getFavorites()
+  }, [])
+
+  return (
+    <FavoriteContext.Provider value={{ favorites, toggle }}>
+      {children}
+    </FavoriteContext.Provider>
+  )
+}
+
+export function useFavorites() {
+  const context = useContext(FavoriteContext)
+
+  return context
+}
