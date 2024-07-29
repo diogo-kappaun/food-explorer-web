16b5fb50a8f25c9bc226291fa37b4c32d110bf64
diogo-kappaun <diogohkappaun@gmail.com>
2024-07-29 19:12:17 -0300

    :lipstick: feat: Add height full

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,7 @@
 export function Section({ children }) {
   return (
-    <div className="overflow-auto px-8 pb-12 pt-24 lg:pt-8">
-      <div className="mx-auto w-page space-y-6">{children}</div>
+    <div className="h-full overflow-auto px-8 pb-12 pt-24 lg:pt-8">
+      <div className="mx-auto h-full w-page space-y-6">{children}</div>
     </div>
   )
 }
