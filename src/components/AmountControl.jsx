9e157a6d8988d947ae002aed5a422ac091847d16
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-01 19:08:20 -0300

    :lipstick: fix: Style adjustment for smaller screens

--- src/components/AmountControl.jsx
+++ src/components/AmountControl.jsx
@@ -4,13 +4,13 @@ import { Button } from './Button'
 
 export function AmountControl({ amount, onIncrease, onDescrease }) {
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex w-full flex-row justify-center gap-2 border-y p-3 lg:relative lg:justify-start lg:border-none lg:p-0">
+    <div className="fixed bottom-0 left-0 right-0 flex flex-row justify-center gap-6 border-y bg-background p-3 lg:relative lg:justify-start lg:border-none lg:p-0">
       <div className="flex items-center justify-center gap-3">
-        <Button onClick={onDescrease} className="px-3 py-2" variant="ghost">
+        <Button onClick={onDescrease} variant="ghost">
           <PiMinus size={16} className="text-muted-foreground" />
         </Button>
         <span className="w-4 text-center text-sm lg:text-base">{amount}</span>
-        <Button onClick={onIncrease} className="px-3 py-2" variant="ghost">
+        <Button onClick={onIncrease} variant="ghost">
           <PiPlus size={16} className="text-muted-foreground" />
         </Button>
       </div>
