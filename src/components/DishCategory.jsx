375305f5b4eec8186640418afd3ed2e36b88110c
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-03 16:21:25 -0300

    :sparkles: feat: Create Dish Category component

---
+++ src/components/DishCategory.jsx
@@ -0,0 +1,45 @@
+import { useNavigate } from 'react-router-dom'
+import { useFavorites } from '../hooks/favorites'
+
+import { Card } from '../components/Card'
+import { Slider } from '../components/Embla/Slider'
+
+import * as Dish from '../components/Dish'
+
+export function DishCategory({ favorites, category, title }) {
+  const { toggle } = useFavorites()
+  const navigate = useNavigate()
+
+  function handleUpdate(id) {
+    navigate(`/dish/update/${id}`)
+  }
+
+  function handleDetails(id) {
+    navigate(`/dish/details/${id}`)
+  }
+
+  async function handleFavorite(id) {
+    await toggle({ dishId: id })
+  }
+
+  return (
+    <Dish.Root>
+      <Dish.Title>{title}</Dish.Title>
+      <Slider>
+        {category.map((dish) => {
+          return (
+            <div key={dish.id} className="pl-4">
+              <Card
+                data={dish}
+                isFavorite={favorites.find((fav) => fav.id === dish.id)}
+                onFavorite={() => handleFavorite(dish.id)}
+                onDetails={() => handleDetails(dish.id)}
+                onUpdate={() => handleUpdate(dish.id)}
+              />
+            </div>
+          )
+        })}
+      </Slider>
+    </Dish.Root>
+  )
+}
