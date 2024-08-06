422b6e5fd2e3ca3267b13d27ea342d009d70babd
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-05 21:13:19 -0300

    :lipstick: feat: Add motion to section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,17 @@
+import { motion } from 'framer-motion'
+
 export function Section({ children }) {
   return (
     <div className="h-full overflow-auto px-8 pt-24 lg:pt-8">
-      <div className="mx-auto w-page space-y-6 pb-12">{children}</div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.6, delay: 0, ease: [0, 0.71, 0.2, 1.01] }}
+        className="mx-auto w-page space-y-6 pb-12"
+      >
+        {children}
+      </motion.div>
     </div>
   )
 }
