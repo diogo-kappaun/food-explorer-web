95a569076ecdb4db7afe759fe6b4a71501d127da
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-08 20:29:40 -0300

    :sparkles: feat: Create useDebounceClick hook

--- src/hooks/useDebounce.jsx
+++ src/hooks/useDebounce.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export function useDebounce(value, delay) {
   const [debouncedValue, setDebouncedValue] = useState(value)
@@ -14,4 +14,18 @@ export function useDebounce(value, delay) {
   }, [value, delay])
 
   return debouncedValue
+}
+
+export function useDebounceClick(callback, delay) {
+  const debouncedCallback = useCallback(() => {
+    let timeout
+    return (...args) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        callback(...args)
+      }, delay)
+    }
+  }, [callback, delay])
+
+  return debouncedCallback()
 }
