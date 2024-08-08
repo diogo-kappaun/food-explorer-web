edefa3fa88633433a8a32142679f244cc55e4ea6
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-07 22:44:01 -0300

    :lipstick: feat: Add events-none on loading

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,8 @@
-export function Container({ children }) {
+export function Container({ children, isLoading }) {
   return (
-    <div className="h-screen bg-background text-foreground lg:grid lg:grid-cols-page lg:grid-rows-page">
+    <div
+      className={`h-screen bg-background text-foreground lg:grid lg:grid-cols-page lg:grid-rows-page ${isLoading ? 'pointer-events-none' : ''}`}
+    >
       {children}
     </div>
   )
