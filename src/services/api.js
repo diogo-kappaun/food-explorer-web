882b0549acf51cf2f2e652e9c2dfa3ea511ed10c
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-10 16:45:03 -0300

    :sparkles: feat: add environment variables to api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:10000/'
+
 export const api = axios.create({
-  baseURL: 'https://food-explorer-api-qxds.onrender.com',
+  baseURL,
 })
