981e0ba29b2fbd4da8c6f430a20d7280e463df89
diogo-kappaun <diogohkappaun@gmail.com>
2024-08-10 19:05:17 -0300

    :bug: fix: Add template literals to url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:10000/'
+const baseURL = `${import.meta.env.VITE_API_URL}` || 'http://localhost:10000/'
 
 export const api = axios.create({
   baseURL,
